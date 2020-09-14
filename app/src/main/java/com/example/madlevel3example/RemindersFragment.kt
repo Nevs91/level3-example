ef77492e5531ac20354ec188e71a44133d5f7453
Sven van Putten <svenvanputten@gmail.com>
2020-09-14 10:26:37 +0200

    Added a list of reminders and a reminderAdapter to the RemindersFragment

--- app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
+++ app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
@@ -1,17 +1,20 @@
 package com.example.madlevel3example
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import com.example.madlevel3example.adapters.ReminderAdapter
+import com.example.madlevel3example.classes.Reminder
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class RemindersFragment : Fragment() {
+
+    private val reminders = arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +26,11 @@ class RemindersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        TODO("Not yet implemented")
     }
 }
